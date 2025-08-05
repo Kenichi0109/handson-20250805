b1614a28c6dd75302f69007c7a697624bb78791f
your-user-name <your-email-address>
2025-08-05 02:10:05 +0000

    Implemented CI pipeline

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,22 +1,76 @@
 import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_codeconnections as codeconnections } from 'aws-cdk-lib';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
+    // GitHubとの接続作成
     const SourceConnection = new codeconnections.CfnConnection(this, 'CICD_Workshop_Connection', {
-        connectionName: 'CICD_Workshop_Connection',
-        providerType: 'GitHub',
-      });
+      connectionName: 'CICD_Workshop_Connection',
+      providerType: 'GitHub',
+    });
 
+    // パイプライン作成
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'Pipeline',
+      crossAccountKeys: false,
+      pipelineType: codepipeline.PipelineType.V2,
+      executionMode: codepipeline.ExecutionMode.QUEUED,
+    });
+
+    // CodeBuildプロジェクト
+    const codeBuild = new codebuild.PipelineProject(this, 'CodeBuild', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml'),
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
+
+    // ソースステージ（GitHub）
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.CodeStarConnectionsSourceAction({
+          actionName: 'GitHub',
+          owner: 'Kenichi0109',
+          repo: 'CICD_Workshop',
+          output: sourceOutput,
+          branch: 'main',
+          connectionArn: SourceConnection.attrConnectionArn,
+        }),
+      ],
+    });
+
+    // テストステージ
+    pipeline.addStage({
+      stageName: 'Code-Quality-Testing',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Unit-Test',
+          project: codeBuild,
+          input: sourceOutput,
+          outputs: [unitTestOutput],
+        }),
+      ],
+    });
+
+    // 接続情報出力
     new CfnOutput(this, 'SourceConnectionArn', {
-        value: SourceConnection.attrConnectionArn,
-      });
+      value: SourceConnection.attrConnectionArn,
+    });
 
     new CfnOutput(this, 'SourceConnectionStatus', {
-        value: SourceConnection.attrConnectionStatus,
-      });
+      value: SourceConnection.attrConnectionStatus,
+    });
   }
 }
